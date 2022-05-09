5c66af346b60b181dd0d8c8312f76cec0aef49e3
tomii <hellotommybowles@gmail.com>
2022-05-09 20:54:02 +0100

    implement equation to calculate cursor's position relative to player

--- include/UIElements/Cursor.hpp
+++ include/UIElements/Cursor.hpp
@@ -3,6 +3,10 @@
 #include "UIElement.hpp"
 
 class Cursor : public UIElement {
+	float deg;
+	sf::Vector2f lastMousePos;
+	bool mouseMoved;
+	void Setup();
 public:
 	Cursor();
 	Cursor(float sizeX, float sizeY);
--- src/UIElements/Cursor.cpp
+++ src/UIElements/Cursor.cpp
@@ -1,23 +1,33 @@
 #include <iostream>
+#include <math.h>
 
 #include "Game.hpp"
 #include "Tools.hpp"
+#include "Events.hpp"
 #include "UIElements/Cursor.hpp"
 #include "Entities/Player.hpp"
 #include "SFML/Graphics.hpp"
 
 Cursor::Cursor(){
-	name = "cursor";
-	setTexture("cursor.png");
+	Setup();
 }
 
 Cursor::Cursor(float sizeX, float sizeY) {
-	name = "cursor";
 	this->sizeX = sizeX;
 	this->sizeY = sizeY;
+	Setup();
+}
+
+void Cursor::Setup() {
+	name = "cursor";
 	setTexture("cursor.png");
+	Events::HookTo("MouseMoved", [this]()->void { mouseMoved = true; });
+	deg = 0;
 }
 
 void Cursor::Update() {
-	sprite.setPosition(Game::player.getPosition() + sf::Vector2f(70, 30));
+	sf::Vector2f playerCent = Game::player.getCenter();
+	setPosition(playerCent + sf::Vector2f(50 * sin(deg), -(50 * cos(deg))));
+	deg += 0.05;
+	if (deg == 360) deg = 0;
 } 
