683492160dc16966702726c97eb247980543cddf
tomii <hellotommybowles@gmail.com>
2022-05-12 09:38:20 +0100

    add screen center to properties

--- include/Properties.hpp
+++ include/Properties.hpp
@@ -1,8 +1,11 @@
 #ifndef PROPERTIES
 #define PROPERTIES
 
+#include "SFML/Graphics.hpp"
+
 namespace Properties {
 	static int frameRate = 144;
+	static sf::Vector2f screenCenter(400, 300);
 }
 
 #endif
