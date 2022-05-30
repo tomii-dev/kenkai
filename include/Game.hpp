c23ab65b5c10a5c64ea0473aaff52b8afd3d4118
tomii <tomii-dev@gmail.com>
2022-05-30 18:45:12 +0100

    GameState enum

--- include/Game.hpp
+++ include/Game.hpp
@@ -5,18 +5,22 @@
 #include "AnimatedEntity.hpp"
 #include "UIElements/Cursor.hpp"
 #include "Camera.hpp"
+#include "State.hpp"
 
 class Player;
+
+enum GameState { MENU, PAUSED, PLAYING };
 
 class Game{
 	static Game* s_instance;
 	bool m_ready;
 	int m_frame;
 	int m_totalFrame;
+	GameState m_state;
 	sf::RenderWindow m_window;
 	Camera m_camera;
 	GameWorld m_world;
-	Cursor* m_cursor;
+	Cursor m_cursor;
 public:
 	Game();
 	static Game& getInstance();
@@ -24,6 +28,7 @@ public:
 	sf::RenderWindow& getWindow();
 	int getFrame();
 	int getTotalFrame();
+	GameState getState();
 	bool isReady();
 	Player* player;
 };
