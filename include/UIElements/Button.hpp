6793fa03f242bc59fa25099446caa62f97056bc7
tomii <tomii-dev@gmail.com>
2022-06-10 08:08:07 +0100

    use vector for size parameter over individual floats

--- include/UIElements/Button.hpp
+++ include/UIElements/Button.hpp
@@ -1,13 +1,18 @@
+#pragma once
+
 #include "UIElement.hpp"
 #include <string>
+#include <functional>
 
 class Button : public UIElement{
     std::string m_text;
     bool m_mousePressed;
-    void Setup();
+    std::function<void()> m_action;
+    void Setup() override;
 public:
     Button();
-    Button(const std::string& text, float sizeX, float sizeY);
-    Button(float sizeX, float sizeY);
+    Button(const std::string& text, sf::Vector2f size, sf::Vector2f position);
+    Button(const std::string& text, sf::Vector2f size, sf::Vector2f position, std::function<void()> f);
     void Update() override;
+    void setAction(std::function<void()> f);
 };
--- src/UIElements/Button.cpp
+++ src/UIElements/Button.cpp
@@ -9,33 +9,43 @@ Button::Button(){
     Setup();
 }
 
-Button::Button(const std::string& text, float sizeX, float sizeY)
+Button::Button(const std::string& text, sf::Vector2f size, sf::Vector2f position, std::function<void()> f)
     : m_text(text)
 {
-    m_sizeX = sizeX;
-    m_sizeY = sizeY;
+    m_sizeX = position.x;
+    m_sizeY = position.y;
+    m_action = f;
     Setup();
 }
 
-Button::Button(float sizeX, float sizeY){
-    m_sizeX = sizeX;
-    m_sizeY = sizeY;
+Button::Button(const std::string& text, sf::Vector2f size, sf::Vector2f position)
+    : m_text(text)
+{
+    m_sizeX = position.x;
+    m_sizeY = position.y;
     Setup();
 }
 
 void Button::Setup(){
-    setTexture("button.png");
-    sf::Vector2f cent = sf::Vector2f(Game::getInstance().getWindow().getSize().x / 2.f, Game::getInstance().getWindow().getSize().y / 2.f);
-    setPosition(cent);
+    name = "button";
+    m_type = ElementType::Button;
+    m_mousePressed = false;
     Events::HookTo(MousePressed, [this](){ m_mousePressed = true; });
+    UIElement::Setup();
+}
+
+void Button::setAction(std::function<void()> f){
+    m_action = f;
 }
 
 void Button::Update(){
     sf::Vector2f mousePos = Tools::getMousePosition();
     if (Tools::InRange(mousePos, getCenter(), sf::Vector2f(m_sizeX, m_sizeY))){
         setTexture("button_darkened.png");
-        if(m_mousePressed)
-            std::cout << "clicked!";
+        if(m_mousePressed){
+            m_action();
+            m_mousePressed = false;
+        }
     }
     else setTexture("button.png");
 }
