c6590017b353cdef446830abe40d2614543027cc
tomii <tomii-dev@gmail.com>
2022-06-10 07:59:02 +0100

    make AddElement method public to allow for menu creation in DataBuilder program, change window references to pointers as we will need to create a menu with no window

--- include/Menu.hpp
+++ include/Menu.hpp
@@ -3,16 +3,43 @@
 #include "SFML/Graphics.hpp"
 #include "UIElement.hpp"
 #include "UIElements/Cursor.hpp"
+#include "UIElements/Button.hpp"
 #include <vector>
+#include <string>
+#include <boost/variant.hpp>
+#include <boost/serialization/variant.hpp>
+#include <boost/serialization/vector.hpp>
+#include <boost/serialization/map.hpp>
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+
+#define MENU_PATH "assets/data/menus/"
+
+using namespace boost;
+typedef variant<float, std::string> ArgValue;
+typedef std::map<std::string, ArgValue> UIElementArgs;
+
+struct MenuConfig{
+    std::vector<int> elements;
+    std::vector<UIElementArgs> elementArgs;
+    template<typename Archive>
+    void serialize(Archive& ar, const unsigned int version) {
+        ar& elements;
+        ar& elementArgs;
+    }
+};
 
 class Menu{
-    sf::RenderWindow& m_window;
-    std::vector<UIElement*> m_elements;
+    sf::RenderWindow* m_window;
+    std::vector<UIElement> m_elements;
     Cursor m_cursor;
-protected:
-    void AddElement(UIElement* element);
+    void load(const std::string& id);
+    Menu(sf::RenderWindow* window, const std::vector<UIElement> elements);
+    friend class MenuBuilder;
 public:
-    Menu(sf::RenderWindow& window);
+    Menu();
+    Menu(sf::RenderWindow* window, const std::string& id);
     ~Menu();
     void Render();
+    void AddElement(UIElement element);
 };
--- src/Menu.cpp
+++ src/Menu.cpp
@@ -1,28 +1,61 @@
-#include <iostream>
 #include "Menu.hpp"
+#include <fstream>
 
-Menu::Menu(sf::RenderWindow& window)
+#include "SFML/Graphics.hpp"
+
+Menu::Menu() : m_window(nullptr)
+{}
+
+Menu::Menu(sf::RenderWindow* window, const std::vector<UIElement> elements)
+    : m_window(window),
+    m_elements(elements),
+    m_cursor(20, 20)
+{}
+
+Menu::Menu(sf::RenderWindow* window, const std::string& id)
     : m_window(window),
     m_cursor  (20, 20)
 {
-    AddElement(&m_cursor);
+    m_elements.push_back(m_cursor);
+    load(id);
 }
 
 Menu::~Menu(){
-    std::vector<UIElement*>::iterator it;
-    for(it = m_elements.end(); it != m_elements.begin(); it++){
-        delete *it;
-        m_elements.pop_back();
-    }
 }
 
-void Menu::AddElement(UIElement* element){
-    m_elements.push_back(element);
+void Menu::load(const std::string& id){
+    std::ifstream ifs(MENU_PATH + id + ".dat", std::ios::binary);
+    boost::archive::binary_iarchive iarch(ifs);
+    MenuConfig conf;
+    iarch >> conf;
+    std::vector<UIElement> temp;
+    int i = 0;
+    for(int e : conf.elements){
+        sf::Vector2f size(
+            get<float>(conf.elementArgs[i]["sizeX"]),
+            get<float>(conf.elementArgs[i]["sizeY"])
+        );
+        sf::Vector2f position(
+            get<float>(conf.elementArgs[i]["posX"]),
+            get<float>(conf.elementArgs[i]["posY"])
+        );
+        switch(ElementType(e)){
+            case ElementType::Button:
+                m_elements.push_back(Button("", size, position));
+                break;
+        }
+        ++i;
+    }
+    Menu(m_window, temp);
 }
 
 void Menu::Render(){
-    for (UIElement* e : m_elements) {
-        e->Update();
-        e->DrawTo(m_window);
+    for (UIElement e : m_elements) {
+        e.Update();
+        e.DrawTo(m_window);
     }
+}
+
+void Menu::AddElement(UIElement element){
+    m_elements.push_back(element);
 }
