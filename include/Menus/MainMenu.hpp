670b06ab2e2421d540ebd33010056cf8e0a53d5e
tomii <tomii-dev@gmail.com>
2022-06-10 08:09:22 +0100

    remove derived menu classes in favour of one class that can load data from prebuilt file

--- include/Menus/MainMenu.hpp
+++
@@ -1,7 +0,0 @@
-#include "SFML/Graphics.hpp"
-#include "Menu.hpp"
-
-class MainMenu : public Menu{
-public:
-    MainMenu(sf::RenderWindow& window);
-};
--- src/Menus/MainMenu.cpp
+++
@@ -1,9 +0,0 @@
-#include "Menus/MainMenu.hpp"
-#include "UIElements/Button.hpp"
-
-MainMenu::MainMenu(sf::RenderWindow& window)
-    : Menu(window)
-{
-    AddElement(new Button(300.f, 119.f));
-    
-}
