78488541a7afc65ddaf26ce4e84848a559c97992
tomii <tomii-dev@gmail.com>
2022-06-20 07:48:00 +0100

    init assets, e.g. fonts, for use throughout project

--- include/Setup.hpp
+++ include/Setup.hpp
@@ -1,8 +1,17 @@
 #include "Events.hpp"
+#include "Assets.hpp"
+#include "Game.hpp"
+
+#include <iostream>
 
 class Setup {
 public:
 	Setup() {
+		Assets::initAssets();
 		Events::RegisterEvents();
+		// hook events
+		Events::HookTo(StartPressed, [](){
+			Game::getInstance().setState(PLAYING);
+		});
 	}
 };
