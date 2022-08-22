66f3f670229250bddc2d3d10a3dafc50e619b8d6
tomii <hellotommybowles@gmail.com>
2022-08-22 17:03:22 +0100

    handle state, velocity, and weight in Object base class
    - protected methods to allow derived classes to make changes to state
    - take in delta time as argument in update
    - sprite movement in update method
    - getter for weight and move state getter into object
    - position getter that returns position in game space, rather than screen
      space, as SFML uses pixel coords

--- kenkai/include/object.hpp
+++ kenkai/include/object.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "state.hpp"
 #include "types.hpp"
 
 class World;
@@ -9,22 +10,30 @@ class Object
 {
 private:
     const char* m_name;
+    State m_state;
     sf::Texture m_texture;
+    float m_weight = 20.f;
     std::vector<Task> m_tasks;
 protected:
+    Vec2 m_velocity;
     sf::Sprite m_sprite;
     World* m_world;
     void setTexture(const char* path);
+    void setState(State state);
+    void addState(State state);
 
     // function to be used by derivative classes to add
     // tasks to the underlying object's task vector
-    void addTask(unsigned int duration, Func f, Func endF = nullptr);
+    void addTask(int duration, Func f, Func endF = nullptr);
 
 public:
     Object(const char* name, World* world = nullptr);
-    virtual void update();
+    virtual void update(int deltaTime);
     const sf::Sprite& getSprite();
     const char* getName();
+    Vec2 getPosition();
     void setPosition(float x, float y);
     void move(float x, float y);
+    float getWeight();
+    State getState();
 };
--- kenkai/src/object.cpp
+++ kenkai/src/object.cpp
@@ -11,30 +11,69 @@ Object::Object(const char* name, World* world)
 
 void Object::setTexture(const char* path)
 {
-    m_texture.loadFromFile(path, sf::IntRect(200, 200, 100, 100));
+    m_texture.loadFromFile(path);
     m_sprite.setTexture(m_texture);
 }
 
-void Object::update()
+void Object::update(int deltaTime)
 {
-    // run tasks
+    // update state
+    if (m_sprite.getPosition().y < m_world->getGroundLevel())
+        setState(OBJECT_INAIR);
+    else
+        setState(OBJECT_ONGROUND);
+
+    if (m_velocity.x > 0)
+        addState(OBJECT_MOVINGRIGHT);
+    else
+        addState(OBJECT_MOVINGLEFT);
+
+    // run tasks 
     std::vector<Task>::iterator it;
     for(it = m_tasks.begin(); it != m_tasks.end();)
     {
         it->func();
-        std::cout << Game::getFrame() << std::endl;
-        if(Game::getFrame() >= it->startFrame + it->duration)
+        if(Game::getTime() >= it->start + it->duration)
         {
             if(it->endFunc) it->endFunc();
             it = m_tasks.erase(it);
         } else it++;
     }
+
+    m_sprite.move(m_velocity * (float)deltaTime);
+    m_velocity = Vec2();
 }
 
-void Object::addTask(unsigned int duration, Func f, Func endF)
+float Object::getWeight()
 {
-    const unsigned int frame = Game::getFrame();
-    m_tasks.push_back(Task{ frame, duration, f, endF });
+    return m_weight;
+}
+
+State Object::getState()
+{
+    return m_state;
+}
+
+void Object::setState(State state)
+{
+    m_state = state;
+}
+
+void Object::addState(State state)
+{
+    m_state |= state;
+}
+
+void Object::addTask(int duration, Func f, Func endF)
+{
+    const int time = Game::getTime();
+    m_tasks.push_back(Task{ time, duration, f, endF });
+}
+
+Vec2 Object::getPosition()
+{
+    const Vec2 spritePos(m_sprite.getPosition());
+    return Vec2(spritePos.x, -spritePos.y);
 }
 
 void Object::setPosition(float x, float y)
