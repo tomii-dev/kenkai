0457833bfd79ecb52125a3c572f6cc9e261376b8
tomii <me@tomii.dev>
2022-10-03 20:46:48 +0100

    animator: divide elapsed time by interval to get current frame

--- kenkai/src/component/animator.cpp
+++ kenkai/src/component/animator.cpp
@@ -12,7 +12,7 @@ void Animator::update(const sf::Time& delta)
         m_elapsed = sf::Time();
 
     // current frame of anim
-    const int32_t ind = floor(m_animation.duration / m_interval);
+    const int32_t ind = floor(m_elapsed.asMilliseconds() / m_interval);
     const Frame frame = m_animation.frames[ind];
 
     if(ind == m_lastFrame)
@@ -23,4 +23,9 @@ void Animator::update(const sf::Time& delta)
     sf::Texture tex;
     tex.loadFromMemory(frame.data, frame.size);
     m_parent->m_sprite.setTexture(tex);
+}
+
+bool Animator::load(const std::string& path)
+{
+    
 }
