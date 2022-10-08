94d08ddfcdc7a8c920d9ba1a4af66b9bff90cbc9
tommy bowles <tommy.bowles@disguise.one>
2022-10-08 14:57:19 +0100

    rm old object files

--- kenkai/src/object.cpp
+++
@@ -1,9 +0,0 @@
-#include "object.hpp"
-
-/* virtual */ void Object::update(const sf::Time& delta)
-{
-    for(const auto& component : m_components)
-        component->update(delta);
-    
-    m_sprite.move(m_velocity);
-}
