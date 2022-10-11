5a945cd27d750231ed51bdc25353a0bd29075ba0
tomii <me@tomii.dev>
2022-10-11 21:15:05 +0100

    fileinterface: remove redundant SECT_END identifier

--- filebuilder/include/fileinterface.hpp
+++ filebuilder/include/fileinterface.hpp
@@ -5,16 +5,15 @@ class FileInterface
 protected:
     static constexpr const char SECT_ID         = char(-1);
     static constexpr const char SECT_START      = char(-2);
-    static constexpr const char SECT_END        = char(-3);
-    static constexpr const char FIELD_ID        = char(-4);
-    static constexpr const char FIELD_TYPE      = char(-5);
-    static constexpr const char FIELD_STR       = char(-6);
-    static constexpr const char FIELD_INT       = char(-7);
-    static constexpr const char FIELD_UINT      = char(-8);
-    static constexpr const char FIELD_ARR       = char(-9);
-    static constexpr const char ARR_STR         = char(-10);
-    static constexpr const char ARR_INT         = char(-11);
-    static constexpr const char ARR_UINT        = char(-12);
-    static constexpr const char ARR_UCHAR       = char(-13);
-    static constexpr const char ARR_BREAK       = char(-14);
+    static constexpr const char FIELD_ID        = char(-3);
+    static constexpr const char FIELD_TYPE      = char(-4);
+    static constexpr const char FIELD_STR       = char(-5);
+    static constexpr const char FIELD_INT       = char(-6);
+    static constexpr const char FIELD_UINT      = char(-7);
+    static constexpr const char FIELD_ARR       = char(-8);
+    static constexpr const char ARR_STR         = char(-9);
+    static constexpr const char ARR_INT         = char(-10);
+    static constexpr const char ARR_UINT        = char(-11);
+    static constexpr const char ARR_UCHAR       = char(-12);
+    static constexpr const char ARR_BREAK       = char(-13);
 };
