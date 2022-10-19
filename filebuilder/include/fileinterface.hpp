4e1a7d49338b94f409db146e55b3fc769cbf6c66
tomii <me@tomii.dev>
2022-10-19 09:01:20 +0100

    fileinterface: use 3 char long strs for separators
    - using single chars allowed for data to be mistaken for separators
    
    * SeparatorType enum to allow for easy switch on separator
    * map of string:SeparatorType to get type from read separator
    * use one char for field type values and arr type values, the reader
      will be in STATE_FIELD_TYPE or STATE_ARR_TYPE state so is safe

--- filebuilder/include/fileinterface.hpp
+++ filebuilder/include/fileinterface.hpp
@@ -1,19 +1,52 @@
 #pragma once
 
+#include <map>
+#include <string>
+
 class FileInterface
 {
 protected:
-    static constexpr const char SECT_ID         = char(-1);
-    static constexpr const char SECT_START      = char(-2);
-    static constexpr const char FIELD_ID        = char(-3);
-    static constexpr const char FIELD_TYPE      = char(-4);
-    static constexpr const char FIELD_STR       = char(-5);
-    static constexpr const char FIELD_INT       = char(-6);
-    static constexpr const char FIELD_UINT      = char(-7);
-    static constexpr const char FIELD_ARR       = char(-8);
-    static constexpr const char ARR_STR         = char(-9);
-    static constexpr const char ARR_INT         = char(-10);
-    static constexpr const char ARR_UINT        = char(-11);
-    static constexpr const char ARR_UCHAR       = char(-12);
-    static constexpr const char ARR_BREAK       = char(-13);
+
+    // all separators are 3 chars long
+    static constexpr const int SEP_SIZE = 3;
+
+    static constexpr const char SECT_ID[]       = "?SI";
+    static constexpr const char SECT_START[]    = "?SS";
+    static constexpr const char SECT_END[]      = "?SE";
+    static constexpr const char FIELD_ID[]      = "?FI";
+    static constexpr const char FIELD_TYPE[]    = "?FT";
+    static constexpr const char FIELD_END[]     = "?FE";
+    static constexpr const char ARR_START[]     = "?AS";
+    static constexpr const char ARR_BREAK[]     = "?AB";
+
+    enum SeparatorType
+    {
+        SPTR_SECT_ID,
+        SPTR_SECT_START,
+        SPTR_SECT_END,
+        SPTR_FIELD_ID,
+        SPTR_FIELD_TYPE,
+        SPTR_FIELD_END,
+        SPTR_ARR_BREAK
+    };
+
+    const std::map<std::string, SeparatorType> m_separators = {
+        {SECT_ID, SPTR_SECT_ID},
+        {SECT_START, SPTR_SECT_START},
+        {SECT_END, SPTR_SECT_END},
+        {FIELD_ID, SPTR_FIELD_ID},
+        {FIELD_TYPE, SPTR_FIELD_TYPE},
+        {FIELD_END, SPTR_FIELD_END},
+        {ARR_BREAK, SPTR_ARR_BREAK}
+    };
+
+    static constexpr const char FIELD_STR       = char(-1);
+    static constexpr const char FIELD_INT       = char(-2);
+    static constexpr const char FIELD_UINT      = char(-3);
+    static constexpr const char FIELD_ARR       = char(-4);
+
+    static constexpr const char ARR_STR         = char(-5);
+    static constexpr const char ARR_INT         = char(-6);
+    static constexpr const char ARR_UINT        = char(-7);
+    static constexpr const char ARR_UCHAR       = char(-8);
 };
