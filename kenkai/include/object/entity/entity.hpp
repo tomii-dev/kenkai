75d6fde788b11b2f34a45040fa56d2275ad5ab60
tomii <me@tomii.dev>
2022-10-22 17:14:58 +0100

    entity: health getter, update override, jump method
    * bool member to determine whether entity is jumping

--- kenkai/include/object/entity/entity.hpp
+++ kenkai/include/object/entity/entity.hpp
@@ -2,9 +2,16 @@
 
 #include "object/object.hpp"
 
+// should never be instantiated directly
 class Entity : public Object
 {
 public:
+    inline const uint32_t& health() { return m_health; }
+
+    virtual void update(const sf::Time& delta) override;
+
+    void jump();
 private:
     uint32_t m_health;
+    bool m_jumping;
 };
