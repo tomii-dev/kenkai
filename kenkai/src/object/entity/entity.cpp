73623351e13bbb041715a0fb90c2186512fe4235
tomii <me@tomii.dev>
2022-10-22 17:20:14 +0100

    entity: use setVelocityY to set velocity for jump

---
+++ kenkai/src/object/entity/entity.cpp
@@ -0,0 +1,19 @@
+#include "object/entity/entity.hpp"
+
+void Entity::update(const sf::Time& delta)
+{
+    if (m_jumping)
+        setVelocityY(.2);
+
+    Object::update(delta);
+}
+
+void Entity::jump()
+{
+    if (m_jumping)
+        return;
+
+    m_jumping = true;
+
+    addTask([this]() { m_jumping = false; }, 200);
+}
