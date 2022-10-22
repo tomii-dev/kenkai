d330bdb1fea75ea484c98392afe6cd5ac9fe407d
tomii <me@tomii.dev>
2022-10-22 16:51:09 +0100

    animator: move ctor to cpp
    * sf::Image property in Frame
    * texture as member to handle lifetime

--- kenkai/include/component/animator.hpp
+++ kenkai/include/component/animator.hpp
@@ -2,20 +2,21 @@
 
 #include "component.hpp"
 
+#include <SFML/Graphics.hpp>
 #include <vector>
 
 class Animator : public Component
 {
 public:
-    inline Animator(Object* parent) : Component(parent) {}
+    Animator(Object* parent);
 
     void update(const sf::Time& delta) override;
     bool load(const std::string& path);
 private:
     struct Frame
     {
-        unsigned char* data;
-        size_t size;
+        const sf::Image img;
+        const uint32_t duration;
     };
 
     struct Animation
@@ -25,6 +26,8 @@ private:
     } m_animation;
     
     sf::Time m_elapsed;
-    uint32_t m_interval;
+    uint32_t m_currentFrame;
     int32_t m_lastFrame;
+
+    sf::Texture m_tex;
 };
