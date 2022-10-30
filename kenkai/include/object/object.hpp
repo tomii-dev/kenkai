e30748f327621eaf47558a3596d6da5c89a8fd3e
tomii <me@tomii.dev>
2022-10-30 19:44:28 +0000

    object: take parent ptr as optional arg in ctor
    * methods for setting object texture
    * move setPosition impls to source, need to account for relative position to parent
    * rename position getter to worldPosition for clarity
    * addChild template method
    * getComponent template method to allow us to grab object's components after adding them
    * vector of child object unique ptrs
    * add Collider as friend
    * update children

--- kenkai/include/object/object.hpp
+++ kenkai/include/object/object.hpp
@@ -4,10 +4,13 @@
 #include "component/component.hpp"
 
 #include <functional>
+#include <algorithm>
 
 class Object
 {
 public:
+    Object(Object* parent = nullptr);
+
     enum State
     {
         OBJECT_ONGROUND,
@@ -19,17 +22,30 @@ public:
 
     inline void render(sf::RenderWindow& window) { window.draw(m_sprite); }
 
-    inline void setPosition(const Vec2& pos) { m_sprite.setPosition(pos); }
+    inline void setTexture(const sf::Texture& tex) { m_tex = tex; m_sprite.setTexture(m_tex); }
+    inline void setTexture(const sf::Image& img) { m_tex.loadFromImage(img); m_sprite.setTexture(m_tex); }
+
+    void setPosition(const Vec2& pos);
+    void setPosition(float x, float y);
     inline virtual void move(int x, int y) { m_sprite.move(x, -y); }
     inline void setVelocity(int x, int y) { m_velocity = Vec2(x, y); }
     inline void setVelocityX(int x) { m_velocity.x = x; }
     inline void setVelocityY(int y) { m_velocity.y = -y; }
 
-    inline const Vec2& position() { return m_position; }
+    inline const Vec2& worldPosition() { return Vec2(m_sprite.getPosition().x, -m_sprite.getPosition().y); }
     inline const uint32_t& weight() { return m_weight; }
     inline const State& state() { return m_state; }
 
-    template<typename ComponentType, typename... Args>
+    template <typename ObjectType, typename ...Args>
+    ObjectType* addChild(Args... args)
+    {
+        static_assert(std::is_base_of<Object, ObjectType>::value);
+
+        m_children.push_back(std::make_unique<ObjectType>(this, args...));
+        return dynamic_cast<ObjectType*>(m_children.back().get());
+    }
+
+    template <typename ComponentType, typename... Args>
     ComponentType* addComponent(Args... args)
     {
         static_assert(std::is_base_of<Component, ComponentType>::value);
@@ -37,18 +53,35 @@ public:
         m_components.push_back(std::make_unique<ComponentType>(this, args...));
         return dynamic_cast<ComponentType*>(m_components.back().get());
     }
+
+    template <typename ComponentType>
+    ComponentType* getComponent()
+    {
+        static_assert(std::is_base_of<Component, ComponentType>::value);
+
+        for (const auto& com : m_components)
+            if (ComponentType* component = dynamic_cast<ComponentType*>(com.get()))
+                return component;
+
+        return nullptr;
+    }
 protected:
     Vec2 m_velocity;
 
     void addTask(std::function<void()> f, uint32_t ms);
 private:
+    Object* m_parent;
     State m_state;
     sf::Sprite m_sprite;
     uint32_t m_weight;
-    Vec2 m_position;
+    sf::Texture m_tex;
 
     using Components = std::vector<std::unique_ptr<Component>>;
     Components m_components;
 
+    using Children = std::vector<std::unique_ptr<Object>>;
+    Children m_children;
+
     friend class Animator;
+    friend class Collider;
 };
--- kenkai/src/object/object.cpp
+++ kenkai/src/object/object.cpp
@@ -3,13 +3,43 @@
 #include "game.hpp"
 #include "util/taskmanager.hpp"
 
+Object::Object(Object* parent) 
+    : m_parent(parent)
+    , m_weight(0)
+{}
+
 /* virtual */ void Object::update(const sf::Time& delta)
 {
-    for(const auto& component : m_components)
+    for (const std::unique_ptr<Object>& child : m_children)
+        child->update(delta);
+
+    for(const std::unique_ptr<Component>& component : m_components)
         component->update(delta);
     
     m_sprite.move(m_velocity * delta.asSeconds());
     m_velocity = Vec2();
+}
+
+void Object::setPosition(const Vec2& pos)
+{
+    if (m_parent)
+    {
+        m_sprite.setPosition(m_parent->worldPosition() + Vec2(pos.x, -pos.y));
+        return;
+    }
+
+    m_sprite.setPosition(Vec2(pos.x, -pos.y));
+}
+
+void Object::setPosition(float x, float y)
+{
+    if (m_parent)
+    {
+        m_sprite.setPosition(m_parent->worldPosition() + Vec2(x, -y));
+        return;
+    }
+
+    m_sprite.setPosition(Vec2(x, -y));
 }
 
 void Object::addTask(TaskManager::Callback callback, uint32_t ms)
