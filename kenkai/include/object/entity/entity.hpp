147f6b63172ee05384d488f8cd89de8084cec6f5
tomii <me@tomii.dev>
2022-10-30 19:43:54 +0000

    entity: implement ctor
    * m_canJump field to determine whether can jump and setter
    * store pointer to collider as member to access collision event
    * onCollision and onAttacked methods to be called on collider's event fire
    * virtual attack method
    * look direction field to determine which way to attack when called
    * jump force field
    * currently just loading player anim file for testing
    * subscribe to onCollision event in collider
    * set look direction based on velocity

--- kenkai/include/object/entity/entity.hpp
+++ kenkai/include/object/entity/entity.hpp
@@ -3,22 +3,40 @@
 #include "object/object.hpp"
 
 class Animator;
+class Collider;
+class Weapon;
 
 // should never be instantiated directly
 class Entity : public Object
 {
 public:
+    Entity();
+
     inline const uint32_t& health() { return m_health; }
+    inline void canJump(bool val) { m_canJump = val; }
 
     virtual void update(const sf::Time& delta) override;
 
+    virtual void attack();
+
     void jump();
 protected:
-    Entity();
-
     Animator* m_animator;
+    Collider* m_collider;
+
     float m_moveSpeed;
+    uint16_t m_jumpForce;
+
+    virtual void onCollision(Object* obj);
+    virtual void onAttacked(Weapon* wep);
 private:
     uint32_t m_health;
     bool m_jumping;
+    bool m_canJump;
+
+    enum LookDirection
+    {
+        LOOK_LEFT,
+        LOOK_RIGHT
+    } m_lookDirection;
 };
--- kenkai/src/object/entity/entity.cpp
+++ kenkai/src/object/entity/entity.cpp
@@ -1,33 +1,74 @@
 #include "object/entity/entity.hpp"
 
 #include "component/animator.hpp"
+#include "component/collider.hpp"
+#include "object/weapon.hpp"
+#include "util/filesys.hpp"
 
-Entity::Entity() : m_animator(addComponent<Animator>())
-{}
+#include <iostream>
+
+Entity::Entity() 
+    : m_animator(addComponent<Animator>())
+    , m_collider(addComponent<Collider>(.75, .75))
+    , m_jumpForce(0)
+    , m_canJump(true)
+{
+    m_animator->load(FileSys::animPath() + "player.ken");
+    m_animator->setDefaultAnimation("idle");
+
+    m_collider->onCollision()->add([this](Object* obj) { onCollision(obj); });
+}
 
 void Entity::update(const sf::Time& delta)
 {
     if (m_jumping)
-        setVelocityY(1);
-
-    if (m_velocity.x < 0)
-        m_animator->setAnimation("walk_left");
-    else if (m_velocity.x > 0)
-        m_animator->setAnimation("walk_right");
+        setVelocityY(m_jumpForce);
     else
-        m_animator->setAnimation("idle");
+        if (m_velocity.x < 0)
+        {
+            m_animator->setAnimation("walk_left");
+            m_lookDirection = LOOK_LEFT;
+        }
+        else if (m_velocity.x > 0)
+        {
+            m_animator->setAnimation("walk_right");
+            m_lookDirection = LOOK_RIGHT;
+        }
+        else
+            m_animator->setAnimation("idle");
 
     m_velocity.x *= m_moveSpeed;
 
     Object::update(delta);
 }
 
+void Entity::attack()
+{
+    if (m_lookDirection == LOOK_LEFT)
+        m_animator->playAnimation("attack_left");
+    else if (m_lookDirection == LOOK_RIGHT)
+        m_animator->setAnimation("attack_right");
+}
+
 void Entity::jump()
 {
-    if (m_jumping)
+    if (m_jumping || !m_canJump)
         return;
 
     m_jumping = true;
+    m_canJump = false;
 
     addTask([this]() { m_jumping = false; }, 200);
+}
+
+void Entity::onCollision(Object* obj)
+{
+    if (Weapon* wep = dynamic_cast<Weapon*>(obj))
+        onAttacked(wep);
+}
+
+void Entity::onAttacked(Weapon* wep)
+{
+    m_animator->setAnimation("attacked");
+    m_health -= wep->damage();
 }
