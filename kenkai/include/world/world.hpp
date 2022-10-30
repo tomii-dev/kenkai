9234973aa4b9bd5a769eb1f8f7991bbe0a6abb51
tomii <me@tomii.dev>
2022-10-30 19:46:21 +0000

    world: use dynamic_cast for object downcast
    * ground and background objects
    * owning pointer to Home
    * non owning pointer to current room
    * Stage struct to randomly generate and store rooms
    * enter method called on enterKenkai event to create stage, and
      the ground and bg for current room
    * compare each object's y position to y pos of ground and pull them down
    * call update and render methods on current room if not null
    * render background and ground

--- kenkai/include/world/world.hpp
+++ kenkai/include/world/world.hpp
@@ -6,6 +6,9 @@
 #include <memory>
 #include <SFML/System.hpp>
 
+#include "room/room.hpp"
+#include "room/home.hpp"
+
 class World
 {
 public:
@@ -13,16 +16,32 @@ public:
 
     void update(const sf::Time& delta);
     void render();
-
+    
     template <typename ObjectType>
     ObjectType* addObject()
     {
         static_assert(std::is_base_of<Object, ObjectType>::value);
 
         m_objects.push_back(std::make_unique<ObjectType>());
-        return static_cast<ObjectType*>(m_objects.back().get());
+        return dynamic_cast<ObjectType*>(m_objects.back().get());
     }
 private:
     sf::RenderWindow& m_window;
     std::vector<std::unique_ptr<Object>> m_objects;
+
+    Object m_ground;
+    Object m_bg;
+
+    std::unique_ptr<Home> m_home;
+    Room* m_room;
+    
+    struct Stage
+    {
+        using RoomList = std::vector<std::unique_ptr<Room>>;
+        RoomList rooms;
+
+        void generate();
+    } m_stage;
+
+    void enter();
 };
--- kenkai/src/world/world.cpp
+++ kenkai/src/world/world.cpp
@@ -1,16 +1,70 @@
 #include "world/world.hpp"
 
-World::World(sf::RenderWindow& window) : m_window(window)
-{}
+#include "../../filebuilder/include/filereader.hpp"
+#include "util/num.hpp"
+#include "object/entity/entity.hpp"
+
+const int g_groundHeight = -200;
+
+World::World(sf::RenderWindow& window) 
+    : m_window(window)
+    , m_home(std::make_unique<Home>())
+{
+    m_home->onEnterKenkai()->add([this]() { enter(); });
+    m_home->onEnterKenkai()->fire();
+}
 
 void World::update(const sf::Time& delta)
 {
+    if(m_room)
+        m_room->update(delta);
+
     for (const std::unique_ptr<Object>& obj : m_objects)
+    {
         obj->update(delta);
+
+        const float grndPos = m_ground.worldPosition().y;
+        const float objPos = obj->worldPosition().y;
+        if (objPos > grndPos)
+            obj->setVelocityY(-400);
+        else
+            if (Entity* ent = dynamic_cast<Entity*>(obj.get()))
+                ent->canJump(true);
+    }
 }
 
 void World::render()
 {
+    if(m_room)
+        m_room->render(m_window);
+
+    m_bg.render(m_window);
+    m_ground.render(m_window);
+
     for (const std::unique_ptr<Object>& obj : m_objects)
         obj->render(m_window);
+}
+
+void World::enter()
+{
+    m_stage.generate();
+    m_room = m_stage.rooms[0].get();
+
+    m_ground = Object();
+    m_ground.setTexture(m_room->groundImg());
+    m_ground.setPosition(0, -m_room->groundY());
+
+    m_bg = Object();
+    m_bg.setTexture(m_room->bgImg());
+}
+
+void World::Stage::generate()
+{
+    for (uint8_t i = 0; i < 4; i++)
+    {
+        const int ind = num::random(0, Room::BIOME_COUNT);
+        const Room::Biome biome = Room::BIOME_PLAINS;
+
+        rooms.push_back(std::make_unique<Room>(biome));
+    }
 }
